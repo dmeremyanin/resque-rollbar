4b021799fb0f41bce4317059f9488754107ac06d
Jon de Andres <jon@wuaki.tv>
2015-01-13 16:54:47 +0100

    Prepare Resque::Failure::Rollbar for 1.4.0.
    Now middlewares should pass :use_exception_level_filters => true option.

--- spec/resque/failure/rollbar_spec.rb
+++ spec/resque/failure/rollbar_spec.rb
@@ -1,14 +1,29 @@
 require 'spec_helper'
 
 describe Resque::Failure::Rollbar do
-  it 'should be notified of an error' do
-    exception = StandardError.new('BOOM')
-    worker    = Resque::Worker.new(:test)
-    queue     = 'test'
-    payload   = { 'class' => Object, 'args' => 89 }
+  let(:exception) { StandardError.new('BOOM') }
+  let(:worker) { Resque::Worker.new(:test) }
+  let(:queue) { 'test' }
+  let(:payload) { { 'class' => Object, 'args' => 89 } }
+  let(:backend) do
+    Resque::Failure::Rollbar.new(exception, worker, queue, payload)
+  end
 
+  it 'should be notified of an error' do
     expect(Rollbar.notifier).to receive(:log).with('error', exception, payload)
-    backend = Resque::Failure::Rollbar.new(exception, worker, queue, payload)
     backend.save
+  end
+
+  context 'with Rollbar version > 1.3' do
+    let(:payload_with_options) { payload.merge(:use_exception_level_filters => true) }
+
+    before do
+      allow(backend).to receive(:rollbar_version).and_return('1.4.0')
+    end
+
+    it 'sends the :use_exception_level_filters option' do
+      expect(Rollbar.notifier).to receive(:error).with(exception, payload_with_options)
+      backend.save
+    end
   end
 end
